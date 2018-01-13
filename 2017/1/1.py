6334972b1acf9f7ee251af84b17fed8a28dc8bcc
Ssuyk <30583950+Ssuyk@users.noreply.github.com>
2018-01-13 16:17:01 -0500

    Update 1.py
    Add some descriptions

--- 2017/1/1.py
+++ 2017/1/1.py
@@ -3,10 +3,11 @@ Advent of code 2017 Day 1
 '''
 
 with open('input1.txt', 'r') as keyfile:
+    # Transform a string file containing a chain of number to a list of integer
     keylist = list(map(int, keyfile.read().strip()))
     print(len(keylist))
     sum = 0
-
+    # 1st part of the answer : if two consecutives numbers are equals, add this number to sum.
     # for i in range(len(keylist)):
     # 	if(keylist[i] == keylist[i-1]):
     # 		sum += keylist[i]
